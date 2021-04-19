036fae352b3f924cb305551df1b43362b87f530e
irerin07 <counterchord89@gmail.com>
2021-04-19 22:12:31 +0900

    인프런 MVC3

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
@@ -28,6 +29,19 @@ public class RequestHeaderServlet extends HttpServlet {
         System.out.println("request.getQueryString() = " + req.getQueryString());
         System.out.println("request.isSecure() = " + req.isSecure()); //https 사용 유무
         System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+
+    private void printHeaders(HttpServletRequest request){
+        System.out.println("--- Headers Start---");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println("headerName = " + headerName);
+        }
+
+        System.out.println("--- Headers -end---");
         System.out.println();
     }
 }
