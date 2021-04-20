e63cf90a4f065729bed48fa80f4d1d10f6967497
irerin07 <counterchord89@gmail.com>
2021-04-21 00:11:43 +0900

    인프런 MVC5

--- src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
 import org.springframework.util.StreamUtils;
 
 import javax.servlet.ServletException;
@@ -14,11 +16,25 @@ import java.nio.charset.StandardCharsets;
 @WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/requestBodyJson")
 public class RequestBodyJsonServlet extends HttpServlet {
 
+    /*
+    JSON 결과를 파싱해서 사용할 수 이쓴 자바 객체로 변환하려면 JSON 변환 라이브러리 필요
+    스프링 부트로 Spring MVC를 선택하면 기본으로 Jackson 라이브러리 제공 (ObjectMapper)
+
+     */
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletInputStream inputStream = req.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
 
         System.out.println("messageBody = " + messageBody);
+
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+
+        System.out.println("helloData.getUsername() = " + helloData.getUsername());
+        System.out.println("helloData.getAge() = " + helloData.getAge());
+
+        resp.getWriter().write("ok");
     }
 }
---
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -0,0 +1,46 @@
+package hello.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/responseHeader")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(HttpServletResponse.SC_OK);
+
+        resp.setHeader("Content-Type", "text/plain;charset=utf-8");
+        resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        resp.setHeader("Pragma", "no-cache");
+        resp.setHeader("my-header", "hello");
+
+        content(resp);
+        cookie(resp);
+        redirect(resp);
+
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+
+    private void content(HttpServletResponse response){
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response) {
+        Cookie cookie  = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);//600sec
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException{
+        response.sendRedirect("/basic/hello-form.html");
+    }
+}
